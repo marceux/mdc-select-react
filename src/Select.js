95db68adea17b08a99b50f8266a22fe44f9e79f6
Marco Salazar de Leon <msalazardeleon@gmail.com>
2017-03-21 16:33:46 -0700

    Completed remainder of updates for simple functionality

--- src/Select.js
+++ src/Select.js
@@ -2,6 +2,7 @@ import React, { PureComponent, PropTypes } from 'react';
 import { Set as ImmutableSet } from 'immutable';
 
 import { MDCSelectFoundation } from '@material/select/dist/mdc.select';
+import { MDCSimpleMenu } from '@material/menu/dist/mdc.menu';
 
 import '@material/list/dist/mdc.list.css';
 import '@material/menu/dist/mdc.menu.css';
@@ -11,10 +12,15 @@ class Select extends PureComponent {
   constructor(props) {
     super(props);
 
+    const { placeholder } = props;
+
     this.state = {
       classes: new ImmutableSet(),
-      tabbable: false,
+      selectedText: placeholder || 'Select...',
     };
+
+    this.optionNodes = {};
+    this.options = [];
 
     // Bind Methods
     this.addClass = this.addClass.bind(this);
@@ -45,7 +51,14 @@ class Select extends PureComponent {
     this.rmAttrForOptionAtIndex = this.rmAttrForOptionAtIndex.bind(this);
     this.registerMenuInteractionHandler = this.registerMenuInteractionHandler.bind(this);
     this.deregisterMenuInteractionHandler = this.deregisterMenuInteractionHandler.bind(this);
+    this.notifyChange = this.notifyChange.bind(this);
     this.getWindowInnerHeight = this.getWindowInnerHeight.bind(this);
+
+    this.getOptionNode = this.getOptionNode.bind(this);
+    this.setRootRef = this.setRootRef.bind(this);
+    this.setMenuRef = this.setMenuRef.bind(this);
+    this.addOptionRef = this.addOptionRef.bind(this);
+    this.renderOption = this.renderOption.bind(this);
 
     // Finally, create our MDC Select Foundation
     this.foundation = new MDCSelectFoundation({
@@ -61,7 +74,7 @@ class Select extends PureComponent {
       makeUntabbable: this.makeUntabbable,
       getComputedStyleValue: this.getComputedStyleValue,
       setStyle: this.setStyle,
-      //create2dRenderingContext: this.create2dRenderingContext,
+      create2dRenderingContext: this.create2dRenderingContext,
       setMenuElStyle: this.setMenuElStyle,
       setMenuElAttr: this.setMenuElAttr,
       rmMenuElAttr: this.rmMenuElAttr,
@@ -77,16 +90,32 @@ class Select extends PureComponent {
       rmAttrForOptionAtIndex: this.rmAttrForOptionAtIndex,
       registerMenuInteractionHandler: this.registerMenuInteractionHandler,
       deregisterMenuInteractionHandler: this.deregisterMenuInteractionHandler,
+      notifyChange: this.notifyChange,
       getWindowInnerHeight: this.getWindowInnerHeight,
     });
   }
 
   componentDidMount() {
+    this.simpleMenu = MDCSimpleMenu.attachTo(this.menuNode);
     this.foundation.init();
   }
 
   componentWillUnmount() {
     this.foundation.destroy();
+    this.simpleMenu = undefined;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // If our options change, reset the optionNodes
+    if (nextProps.options !== this.props.options) {
+      this.optionNodes = {};
+      this.options = [];
+    }
+  }
+
+  getOptionNode(index) {
+    const { id } = this.options[index];
+    return this.optionNodes[id];
   }
 
   addClass(className) {
@@ -102,37 +131,27 @@ class Select extends PureComponent {
   }
 
   setAttr(attr, value) {
-    this.refs.root.setAttribute(attr, value);
+    this.rootNode.setAttribute(attr, value);
   }
 
   rmAttr(attr) {
-    this.refs.root.removeAttribute(attr);
+    this.rootNode.removeAttribute(attr);
   }
 
   computeBoundingRect() {
-    if (this.refs.root) {
-      return this.refs.root.getBoundingClientRect();
-    }
-
-    return { left: 0, top: 0 };
+    return this.rootNode.getBoundingClientRect();
   }
 
   registerInteractionHandler(type, handler) {
-    if (this.refs.root) {
-      this.refs.root.addEventListener(type, handler);
-    }
+    this.rootNode.addEventListener(type, handler);
   }
 
   deregisterInteractionHandler(type, handler) {
-    if (this.refs.root) {
-      this.refs.root.removeEventListener(type, handler);
-    }
+    this.rootNode.removeEventListener(type, handler);
   }
 
   focus() {
-    if (this.refs.root) {
-      this.refs.root.focus();
-    }
+    this.rootNode.focus();
   }
 
   makeTabbable() {
@@ -148,15 +167,11 @@ class Select extends PureComponent {
   }
 
   getComputedStyleValue(propertyName) {
-    if (this.refs.root) {
-      return window.getComputedStyle(this.refs.root)[propertyName];
-    }
+    return window.getComputedStyle(this.rootNode)[propertyName];
   }
 
   setStyle(propertyName, value) {
-    if (this.refs.root) {
-      this.refs.root.style.setProperty(propertyName, value);
-    }
+    this.rootNode.style.setProperty(propertyName, value);
   }
 
   create2dRenderingContext() {
@@ -164,111 +179,149 @@ class Select extends PureComponent {
   }
 
   setMenuElStyle(propertyName, value) {
-    if (this.refs.menu) {
-      this.refs.menu.style.setProperty(propertyName, value);
-    }
+    this.menuNode.style.setProperty(propertyName, value);
   }
 
   setMenuElAttr(attr, value) {
-    if (this.refs.menu) {
-      this.refs.menu.setAttribute(attr, value);
-    }
+    this.menuNode.setAttribute(attr, value);
   }
 
   rmMenuElAttr(attr) {
-    if (this.refs.menu) {
-      this.refs.menu.removeAttribute(attr);
-    }
+    this.menuNode.removeAttribute(attr);
   }
 
   getMenuElOffsetHeight() {
-    if (this.refs.menu) {
-      return this.refs.menu.offsetHeight;
-    }
+    return this.menuNode.offsetHeight;
   }
 
   getOffsetTopForOptionAtIndex(index) {
-    return 0;
+    return this.getOptionNode(index).offsetTop;
   }
 
   openMenu(focusIndex) {
-    // focusIndex = string
+    // this.props.onFocus()
+    this.simpleMenu.foundation_.open(focusIndex);
   }
 
   isMenuOpen() {
-    return true;
+    return this.simpleMenu.foundation_.isOpen();
   }
 
   setSelectedTextContent(selectedTextContent) {
-    return null;
+    this.setState(prevState => ({
+      selectedText: selectedTextContent,
+    }));
   }
 
   getNumberOfOptions() {
-    return 0; 
+    return this.options.length;
   }
 
   getTextForOptionAtIndex(index) {
-    return '';
+    return this.options[index].label;
   }
 
   getValueForOptionAtIndex(index) {
-    return '';
+    return this.options[index].value;
   }
 
   setAttrForOptionAtIndex(index, attr, value) {
-    //
+    this.getOptionNode(index).setAttribute(attr, value);
   }
 
   rmAttrForOptionAtIndex(index, attr) {
-    //
+    this.getOptionNode(index).removeAttribute(attr);
   }
 
   registerMenuInteractionHandler(type, handler) {
-    //
+    this.menuNode.addEventListener(type, handler);
   }
 
   deregisterMenuInteractionHandler(type, handler) {
-    //
+    this.menuNode.removeEventListener(type, handler);
+  }
+
+  notifyChange() {
+    // this.props.onChange()
   }
 
   getWindowInnerHeight() {
     return window.innerHeight;
   }
 
+  setRootRef(node) {
+    this.rootNode = node;
+  }
+
+  setMenuRef(node) {
+    this.menuNode = node;
+  }
+
+  addOptionRef(id) {
+    return (node) => {
+      // If we receive a HTMLElement, add to our
+      // option nodes object, else, delete key
+      if (node) {
+        this.optionNodes[id] = node;
+      } else {
+        delete this.optionNodes[id];
+      }
+    };
+  }
+
+  renderOption({ id, label }) {
+    return (
+      <li
+        className="mdc-list-item"
+        id={id}
+        key={id}
+        ref={this.addOptionRef(id)}
+        role="option"
+        tabIndex="0"
+      >
+        {label}
+      </li>
+    );
+  }
+
   render() {
-    const { classes, tabbable } = this.state;
+    const { options, placeholder } = this.props;
+    const { classes, selectedText } = this.state;
+
+    // Setting the option IDs array
+    const renderedOptions = [];
+
+    // Setting our default option
+    const defaultOption = {
+      id: 'default',
+      label: placeholder || 'Select...',
+      value: '',
+    };
+
+    if (options && Array.isArray(options) && options.length > 0) {
+      // Have our own array of the options
+      this.options = [defaultOption, ...options];
+    } else {
+      this.options = [defaultOption];
+    }
+
+    // Iterate over our class options array
+    this.options.forEach(option => {
+      // Render the option and push to our rendered array
+      renderedOptions.push(this.renderOption(option));
+    });
 
     return (
       <div
         className={`mdc-select ${classes.toJS().join(' ')}`}
-        ref="root"
+        ref={this.setRootRef}
         role="listbox"
-        tabIndex={(tabbable) ? '0' : '-1'}
+        tabIndex="0"
       >
-        <span className="mdc-select__selected-text">Pick a food group</span>
-        <div
-          className="mdc-simple-menu mdc-select__menu"
-          ref="menu"
-        >
+        <span className="mdc-select__selected-text">{selectedText}</span>
+        <div className="mdc-simple-menu mdc-select__menu" ref={this.setMenuRef}>
           <ul className="mdc-list mdc-simple-menu__items">
-            <li className="mdc-list-item" role="option" id="grains" tabIndex="0">
-              Bread, Cereal, Rice, and Pasta
-            </li>
-            <li className="mdc-list-item" role="option" id="vegetables" tabIndex="0">
-              Vegetables
-            </li>
-            <li className="mdc-list-item" role="option" id="fruit" tabIndex="0">
-              Fruit
-            </li>
-            <li className="mdc-list-item" role="option" id="dairy" tabIndex="0">
-              Milk, Yogurt, and Cheese
-            </li>
-            <li className="mdc-list-item" role="option" id="meat" tabIndex="0">
-              Meat, Poultry, Fish, Dry Beans, Eggs, and Nuts
-            </li>
-            <li className="mdc-list-item" role="option" id="fats" tabIndex="0">
-              Fats, Oils, and Sweets
-            </li>
+            {renderedOptions}
           </ul>
         </div>
       </div>
@@ -277,7 +330,7 @@ class Select extends PureComponent {
 }
 
 Select.propTypes = {
-
+  options: PropTypes.array.isRequired,
 };
 
 export default Select;
--- src/Select.test.js
+++ src/Select.test.js
@@ -1,132 +1,242 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import sinon from 'sinon';
 import Select from './Select';
 
 describe('<Select />', () => {
   let component;
+  let infoStub;
   let instance;
+  let options;
+  let stub;
+  let stub2;
 
   beforeAll(() => {
-    component = mount(<Select />);
+    // Hack because the console.info for sinon is STUPID
+    infoStub = sinon.stub(console, 'info', () => {});
 
+    // We have to stub the 2d rendering context call because it uses `document` global
+    stub = sinon.stub(Select.prototype, 'create2dRenderingContext', () => ({
+      font: '',
+      measureText: () => ({width: 0}),
+    }));
+
+    stub2 = sinon.stub(Select.prototype, 'openMenu', () => ({}));
+  });
+
+  beforeEach(() => {
+    options = [
+      { id: 'apple', label: 'Apple', value: 'apple' },
+      { id: 'orange', label: 'Orange', value: 'orange' },
+      { id: 'strawberry', label: 'Strawberry', value: 'strawberry' },
+      { id: 'grape', label: 'Grape', value: 'grape' },
+    ];
+
+    component = mount(<Select options={options} />);
+
+    // Get the latest instance found in the component
     instance = component.instance();
   });
 
-  it('has methods that correspond to the MDCSelectFoundation adapter', () => {
-    const adapterMethods = [
-      'addClass', 'removeClass', 'setAttr', 'rmAttr', 'computeBoundingRect',
-      'registerInteractionHandler', 'deregisterInteractionHandler', 'focus', 'makeTabbable', 'makeUntabbable',
-      'getComputedStyleValue', 'setStyle', 'create2dRenderingContext', 'setMenuElStyle', 'setMenuElAttr',
-      'rmMenuElAttr', 'getMenuElOffsetHeight', 'getOffsetTopForOptionAtIndex', 'openMenu', 'isMenuOpen',
-      'setSelectedTextContent', 'getNumberOfOptions', 'getTextForOptionAtIndex',
-      'getValueForOptionAtIndex', 'setAttrForOptionAtIndex', 'rmAttrForOptionAtIndex',
-      'registerMenuInteractionHandler', 'deregisterMenuInteractionHandler', 'getWindowInnerHeight',
+  afterAll(() => {
+    // Restore the stub after all the tests are done
+    stub.restore();
+    stub2.restore();
+
+    infoStub.restore();
+  });
+
+  it('allows for default selected text via props', () => {
+    // This is the default text
+    expect(component).toHaveState('selectedText', 'Select...');
+
+    // Create new component with placeholder text
+    component = mount(<Select options={options} placeholder={'Select a Fruit'} />);
+
+    expect(component).toHaveState('selectedText', 'Select a Fruit');
+  });
+
+  it('has the appropriate refs', () => {
+    expect(instance.rootNode).toBeDefined();
+    expect(instance.menuNode).toBeDefined();
+  });
+
+  it('accepts a collection to render its options', () => {
+    // Find all of the option elements in the current component
+    const optionEls = component.find('.mdc-list-item');
+
+    // We want to have the same amount of options rendered that we passed
+    expect(optionEls.length).toEqual(options.length + 1);
+
+    // With the children we created in the "beforeAll"
+    options.forEach((option, index) => {
+      // Add one to get current index to account for default
+      const currentIndex = index + 1;
+
+      // Get a specific rendered option
+      const optionEl = optionEls.at(currentIndex);
+
+      // Expect inner text to match label
+      expect(optionEl.text()).toEqual(option.label);
+      expect(optionEl.prop('id')).toEqual(option.id);
+    });
+  });
+
+  it('handles options that are not arrays', () => {
+    // Stubbing console because we don't want any printouts
+    const consoleStub = sinon.stub(console, 'error', () => {});
+
+    // We are providing different types of option args
+    const optionsArgs = [
+      undefined,
+      '1',
+      1,
+      {},
+      [],
     ];
 
-    adapterMethods.forEach(method => {
-      expect(instance[method]).not.toBeUndefined();
+    optionsArgs.forEach(arg => {
+      component = mount(<Select options={arg} />);
+
+      // Get the new instance
+      instance = component.instance();
+
+      // Test all the times we would actually attempt to access option
+      expect(instance.getNumberOfOptions()).toEqual(1);
+      expect(instance.getTextForOptionAtIndex(0)).toEqual('Select...');
+      expect(instance.getValueForOptionAtIndex(0)).toEqual('');
+      expect(component.find('.mdc-list-item').length).toEqual(1);
     });
+
+    consoleStub.restore();
   });
 
-  it('has a reference to the root element', () => {
-    expect(component).toHaveRef('root');
+  it('updates options when they change, not when they do not change', () => {
+    let prevOptions = instance.optionNodes;
+
+    // Only returning the first two options this time
+    component.setProps({
+      options: [
+        options[0],
+        options[1],
+      ],
+    });
+
+    expect(component.find('.mdc-list-item').length).toEqual(3);
+    expect(Object.keys(instance.optionNodes).length).toEqual(3);
+
+    // Set options to old options
+    component.setProps({
+      options: options,
+    });
+
+    expect(component.find('.mdc-list-item').length).toEqual(5);
+    expect(Object.keys(instance.optionNodes).length).toEqual(5);
+
+    // Update our prevOptions
+    prevOptions = instance.optionNodes;
+
+    // Changing a prop that is NOT options
+    component.setProps({
+      selectedText: 'Changing',
+    });
+
+    expect(component.find('.mdc-list-item').length).toEqual(5);
+    expect(Object.keys(instance.optionNodes).length).toEqual(5);
   });
 
-  it('can add/remove classes to/from the root element', () => {
-    instance.addClass('testing');
-    expect(component.ref('root')).toHaveClassName('testing');
+  describe('- MDCSelectFoundation', () => {
+    let foundation;
+    let rootComponent;
+    let rootNode;
 
-    instance.removeClass('testing');
-    expect(component.ref('root')).not.toHaveClassName('testing');
-  });
+    beforeEach(() => {
+      // Get the root component
+      rootComponent = component.find('.mdc-select');
 
-  it('can set/remove attributes and their values from the root element', () => {
-    const DOMNode = component.ref('root').getDOMNode();
+      // Get the dom node for the root component
+      rootNode = instance.rootNode;
 
-    instance.setAttr('width', '1px');
-    expect(DOMNode.getAttribute('width')).toEqual('1px');
+      foundation = instance.foundation;
+    });
 
-    instance.rmAttr('width');
-    expect(DOMNode.getAttribute('width')).toEqual(null);
-  });
+    it('has an adapter whose properties are methods belonging to the component', () => {
+      const adapter = foundation.adapter_;
 
-  describe('- computeBoundingRect', () => {
-    it('calls getBoundingClientRect() on root element to get bounding rect', () => {
-      // Call the method we're testing and look at the output
-      expect(instance.computeBoundingRect()).toMatchObject({
-        left: 0,
-        top: 0,
+      const adapterMethods = [
+        'addClass', 'removeClass', 'setAttr', 'rmAttr', 'computeBoundingRect',
+        'registerInteractionHandler', 'deregisterInteractionHandler', 'focus', 'makeTabbable', 'makeUntabbable',
+        'getComputedStyleValue', 'setStyle', 'create2dRenderingContext', 'setMenuElStyle', 'setMenuElAttr',
+        'rmMenuElAttr', 'getMenuElOffsetHeight', 'getOffsetTopForOptionAtIndex', 'openMenu', 'isMenuOpen',
+        'setSelectedTextContent', 'getNumberOfOptions', 'getTextForOptionAtIndex',
+        'getValueForOptionAtIndex', 'setAttrForOptionAtIndex', 'rmAttrForOptionAtIndex',
+        'registerMenuInteractionHandler', 'deregisterMenuInteractionHandler', 'getWindowInnerHeight',
+      ];
+
+      adapterMethods.forEach(method => expect(instance[method]).toEqual(adapter[method]));
+    });
+
+    it('opens and closes the component', () => {
+      // Open the select
+      foundation.open_();
+
+      expect(rootComponent).toHaveClassName('.mdc-select--open');
+
+      // We are going to spy on the focus method
+      const spy = sinon.spy(rootNode, 'focus');
+
+      // Close the select
+      foundation.close_();
+
+      expect(rootComponent).not.toHaveClassName('.mdc-select--open');
+      expect(spy.called).toEqual(true);
+
+      spy.restore();
+    });
+
+    it('sets/unsets a disabled state', () => {
+      foundation.setDisabled(true);
+
+      expect(rootComponent).toHaveClassName('.mdc-select--disabled');
+      expect(rootNode.getAttribute('tab-index', '-1'));
+      expect(rootNode.getAttribute('aria-disabled')).toEqual('true');
+
+      foundation.setDisabled(false);
+
+      expect(rootComponent).not.toHaveClassName('.mdc-select--disabled');
+      expect(rootNode.getAttribute('tab-index', '0'));
+      expect(rootNode.getAttribute('aria-disabled')).toEqual(null);
+    });
+
+    it('selects an option by index', () => {
+      // Default, we are going to start at -1
+      expect(foundation.selectedIndex_).toEqual(-1);
+
+      // We are going to iterate over all the items and check the values
+      options.forEach((option, index) => {
+        // We are going to choose the first index
+        // We are adding '1' because the system automatically adds a default option
+        foundation.setSelectedIndex(index + 1);
+
+        // get the DOMNode for the option at index 0, and check aria-selected
+        const optionNode = instance.optionNodes[option.id];
+        expect(optionNode.getAttribute('aria-selected')).toEqual('true');
+
+        // Test to see that the selected index changed, and so did label to current option
+        expect(foundation.selectedIndex_).toEqual(index + 1);
+        expect(component).toHaveState('selectedText', option.label);
       });
     });
-  });
 
-  describe('- makeTabbable', () => {
-    it('renders the root element with a tab-index of 0', () => {
-      // the state starts with a tab-index of -1
-      expect(component.find('.mdc-select')).toHaveProp('tabIndex', '-1');
+    it('gets the value of the current selected index', () => {
+      // We are going to choose the first index
+      foundation.setSelectedIndex(-1);
 
-      instance.makeTabbable();
-      expect(component.find('.mdc-select')).toHaveProp('tabIndex', '0');
-    });
-  });
+      expect(foundation.getValue()).toEqual('');
 
-  describe('- makeUntabbable', () => {
-    it('renders the root element with a tab-index of 0', () => {
-      // the state starts with a tab-index of 0 from previous test
-      expect(component.find('.mdc-select')).toHaveProp('tabIndex', '0');
+      foundation.setSelectedIndex(1);
 
-      instance.makeUntabbable();
-      expect(component.find('.mdc-select')).toHaveProp('tabIndex', '-1');
-    });
-  });
-
-  describe('- getComputedStyleValue', () => {
-    it('gets the root elements computed style value of the given propertyName', () => {
-      const DOMNode = component.ref('root').getDOMNode();
-
-      // Manually set the style property value
-      DOMNode.style.setProperty('width', '100px');
-
-      expect(instance.getComputedStyleValue('width')).toEqual('100px');
-    });
-  });
-
-  describe('- setStyle', () => {
-    it('sets the css property, propertyName, to a value on the root element', () => {
-      instance.setStyle('width', '200px');
-
-      // This relies on using the previous method to get the actual property
-      expect(instance.getComputedStyleValue('width')).toEqual('200px');
-    });
-  });
-
-  describe('- setMenuElStyle', () => {
-    it('sets the css property, propertyName, to a value on the menu element', () => {
-      instance.setMenuElStyle('width', '300px');
-
-      const DOMNode = component.ref('menu').getDOMNode();
-
-      expect(DOMNode.style.width).toEqual('300px');
-    });
-  });
-
-  describe('- setMenuElAttr', () => {
-    it('sets an attribute with value on the menu element', () => {
-      instance.setMenuElAttr('height', '300px');
-
-      const DOMNode = component.ref('menu').getDOMNode();
-
-      expect(DOMNode.getAttribute('height')).toEqual('300px');
-    });
-  });
-
-  describe('- rmMenuElAttr', () => {
-    it('removes an attribute from the menu element', () => {
-      instance.rmMenuElAttr('height');
-
-      const DOMNode = component.ref('menu').getDOMNode();
-
-      expect(DOMNode.getAttribute('height')).toEqual(null);
+      expect(foundation.getValue()).toEqual('apple');
     });
   });
 });
