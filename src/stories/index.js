89e5e95bec9791f5cd7b312844465b435306613a
Marco Salazar de Leon <msalazardeleon@gmail.com>
2017-03-21 16:34:00 -0700

    made a storybook!

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { storiesOf, action, linkTo } from '@kadira/storybook';
+import { storiesOf } from '@kadira/storybook';
 import Select from '../Select';
 
+const options = [
+  { id: 'apple', label: 'Apple', value: 'apple' },
+  { id: 'orange', label: 'Orange', value: 'orange' },
+  { id: 'strawberry', label: 'Strawberry', value: 'strawberry' },
+  { id: 'grape', label: 'Grape', value: 'grape' },
+];
+
 storiesOf('Select', module)
-  .add('', () => (
-    <Select />
+  .add('with default options', () => (
+    <Select options={options} placeholder="Select a Fruit" />
   ));
