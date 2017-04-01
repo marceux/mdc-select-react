140fcdfff16e48ac5ceebe9a63aabe57c9200c11
Marco Salazar de Leon <msalazardeleon@gmail.com>
2017-03-31 18:26:14 -0700

    Updated foundation to be less opinionated and not include input register

--- src/foundations/DatalistFoundation.js
+++ src/foundations/DatalistFoundation.js
@@ -6,17 +6,12 @@ class DatalistFoundation extends MDCFoundation {
     return {
       addClass: () => {},
       removeClass: () => {},
-      hasClass: () => {},
-      registerInputEventHandler: () => {},
-      deregisterInputEventHandler: () => {},
-      registerBtnClickHandler: () => {},
-      deregisterBtnClickHandler: () => {},
       registerInteractionEventHandler: () => {},
       deregisterInteractionEventHandler: () => {},
       registerDocumentClickHandler: () => {},
       deregisterDocumentClickHandler: () => {},
-      isInputFocused: () => {},
-      matchExists: () => {},
+      storeFocus: () => {},
+      restoreFocus: () => {},
       focusOnElementAtIndex: () => {},
       getIndexForEventTarget: () => {},
       getNumberOfItems: () => {},
@@ -30,38 +25,22 @@ class DatalistFoundation extends MDCFoundation {
 
     // Create our handlers; this is so that `this` still points to our
     // foundation within the `handle{...}` methods
-    this.inputClickHandler_ = (event) => this.handleInputClick(event);
-    this.inputInputHandler_ = (event) => this.handleInputInput(event);
-    this.inputKeyDownHandler_ = (event) => this.handleInputKeyDown(event);
-    this.btnClickHandler_ = () => this.handleBtnClick(event);
-    this.itemClickHandler_ = (event) => this.handleItemClick(event);
-    this.itemKeyDownHandler_ = (event) => this.handleItemKeyDown(event);
-    this.itemKeyUpHandler_ = (event) => this.handleItemKeyUp(event);
-    this.documentClickHandler_ = () => this.handleDocumentClick();
+    this.itemClickHandler_ = (event) => this.handleItemClick_(event);
+    this.itemKeyDownHandler_ = (event) => this.handleItemKeyDown_(event);
+    this.itemKeyUpHandler_ = (event) => this.handleItemKeyUp_(event);
+    this.documentClickHandler_ = () => this.handleDocumentClick_();
 
     // Default values for component
     this.isOpen_ = false;
   }
 
   init() {
-    this.adapter_.registerInputEventHandler('click', this.inputClickHandler_);
-    this.adapter_.registerInputEventHandler('input', this.inputInputHandler_);
-    this.adapter_.registerInputEventHandler('keydown', this.inputKeyDownHandler_);
-
-    this.adapter_.registerBtnClickHandler(this.btnClickHandler_);
-
     this.adapter_.registerInteractionEventHandler('click', this.itemClickHandler_);
     this.adapter_.registerInteractionEventHandler('keydown', this.itemKeyDownHandler_);
     this.adapter_.registerInteractionEventHandler('keyup', this.itemKeyUpHandler_);
   }
 
   destroy() {
-    this.adapter_.deregisterInputEventHandler('click', this.inputClickHandler_);
-    this.adapter_.deregisterInputEventHandler('input', this.inputInputHandler_);
-    this.adapter_.deregisterInputEventHandler('keydown', this.inputKeyDownHandler_);
-
-    this.adapter_.deregisterBtnClickHandler(this.btnClickHandler_);
-
     this.adapter_.deregisterInteractionEventHandler('click', this.itemClickHandler_);
     this.adapter_.deregisterInteractionEventHandler('keydown', this.itemKeyDownHandler_);
     this.adapter_.deregisterInteractionEventHandler('keyup', this.itemKeyUpHandler_);
@@ -71,22 +50,30 @@ class DatalistFoundation extends MDCFoundation {
     return this.isOpen_;
   }
 
-  open() {
+  open(storeFocus = false) {
     // Register the document click handler
     this.adapter_.registerDocumentClickHandler(this.documentClickHandler_);
 
     this.isOpen_ = true;
     this.adapter_.addClass('mdc-datalist--open');
-    return;
+
+    // If we want, we can store the current focused item
+    if (storeFocus) {
+      this.adapter_.storeFocus();
+    }
   }
 
-  close() {
+  close(restoreFocus = false) {
     // Deregister the document click handler
     this.adapter_.deregisterDocumentClickHandler(this.documentClickHandler_);
 
     this.isOpen_ = false;
     this.adapter_.removeClass('mdc-datalist--open');
-    return;
+
+    // If we want, after closing, we can restore the previously focused item
+    if (restoreFocus) {
+      this.adapter_.restoreFocus();
+    }
   }
 
   toggle() {
@@ -97,119 +84,46 @@ class DatalistFoundation extends MDCFoundation {
     }
   }
 
-  handleInputClick(event) {
-    // Stopping the click propagation
-    event.stopPropagation();
-
-    if (event.target.value !== '' && this.adapter_.matchExists(event.target.value)) {
-      this.open();
-    }
+  focusOnItemAtIndex(index) {
+    this.adapter_.storeFocus();
+    this.adapter_.focusOnElementAtIndex(index);
   }
 
-  handleInputInput(event) {
-    // If the value matches, then we'll make sure it's open,
-    // else we'll close our autocomplete component
-    if (event.target.value !== '' && this.adapter_.matchExists(event.target.value)) {
-      if (!this.isOpen()) {
-        this.open();
-      }
-    } else {
-      this.close();
-    }
-
-    return true;
-  }
-
-  handleInputKeyDown(event) {
-    const { keyCode, key } = event;
-    const isNext = (key === 'ArrowDown' || keyCode === 40);
-    const isPrevious = (key === 'ArrowUp' || keyCode === 38);
-
-    // Only handling keyboard events for the isNext or isPrevious keys
-    if (!isNext && !isPrevious) {
-      return false;
-    }
-
-    // We are going to get the number of items in our data list
-    const numberOfItems = this.adapter_.getNumberOfItems();
-
-    // Set our index variable
-    let index;
-
-    // If we actually have items...
-    if (numberOfItems) {
-      // Check if "isNext", set index to 0
-      if (isNext) {
-        index = 0;
-      }
-
-      // Check if "isPrevious", set index to number - 1
-      if (isPrevious) {
-        index = numberOfItems - 1;
-      }
-
-      // Check if our menu is NOT open, then open
-      if (!this.isOpen()) {
-        this.open();
-      }
-
-      this.adapter_.focusOnElementAtIndex(index);
-    }
-
-    return true;
-  }
-
-  handleBtnClick(event) {
-    // Focus on the input element
-    this.adapter_.focusOnInput();
-
-    // Open/close the autocomplete list
-    this.toggle();
-
-    // We don't want the event triggering our document listener
-    event.stopPropagation();
-  }
-
-  handleItemClick(event) {
-    const index = this.adapter_.getIndexForEventTarget(event.target);
-
-    // If an index value was returned that is AT LEAST 0, then we have
-    // an index and we'll select it!
-    if (index >= 0) {
-      // Restore Focus to Input
-      this.adapter_.focusOnInput();
-
-      this.close();
-
-      // Select the item at the index
-      this.adapter_.selectItemAtIndex(index);
-    }
-
-    return true;
-  }
-
-  handleItemKeyDown(event) {
+  handleItemKeyDown_(event) {
     // Do nothing if Alt, Ctrl or Meta are pressed.
     if (event.altKey || event.ctrlKey || event.metaKey) {
-      return false;
+      return true;
     }
-
-    // Get the keyCode and key
-    const { keyCode, key } = event;
-
-    // Set our tab button check
-    const isTab = (key === 'Tab' || keyCode === 9);
 
     // Get the number of items...
     const numberOfItems = this.adapter_.getNumberOfItems();
 
-    if (numberOfItems <= 1) {
-      // If we only have one item and we are tabbing, we are going to focus
-      // on the input, and close the list
-      if (isTab) {
-        // Restore Focus to Input
-        this.adapter_.focusOnInput();
-        this.close();
+    // Return true and do nothing if no items
+    if (numberOfItems <= 0) {
+      return true;
+    }
+
+    // Then we must have at least 1 number of items, the last item index is that
+    // number minus 1. Also get the current index
+    const lastIndex = numberOfItems - 1;
+    const index = this.adapter_.getIndexForEventTarget(event.target);
+
+    // Get the keyCode and key and shiftKey if it exists
+    const { keyCode, key, shiftKey } = event;
+
+    // Set our tab button check
+    const isTab = (key === 'Tab' || keyCode === 9);
+
+    // If the tab key is pressed...
+    if (isTab) {
+      // If we press shift and we are at the first index, we are leaving the list
+      // and going "back" to the previously active element
+      // Or, if the index is the last index, and we pressed tab, then we are leaving
+      // the list and going "forward", even though it is still the previously
+      // active element
+      if (numberOfItems === 1 || (shiftKey && index === 0) || (index === lastIndex)) {
+        event.preventDefault();
+        this.close(true);
         this.adapter_.notifyCancel();
         return false;
       }
@@ -218,21 +132,22 @@ class DatalistFoundation extends MDCFoundation {
     const isNext = (key === 'ArrowDown' || keyCode === 40);
     const isPrevious = (key === 'ArrowUp' || keyCode === 38);
 
+    // If none of these, do nothing
     if (!isNext && !isPrevious) {
-      return false;
+      return true;
     }
 
-    const lastIndex = numberOfItems - 1;
-    const index = this.adapter_.getIndexForEventTarget(event.target);
+    // We are going to prevent default behavior for both isNext/isPrevious keydowns
+    event.preventDefault();
 
     if (isNext) {
       switch (index) {
         case lastIndex:
           this.adapter_.focusOnElementAtIndex(0);
-          return true;
+          return false;
         default:
           this.adapter_.focusOnElementAtIndex(index + 1);
-          return true;
+          return false;
       }
     }
 
@@ -240,25 +155,18 @@ class DatalistFoundation extends MDCFoundation {
       switch (index) {
         case 0:
           this.adapter_.focusOnElementAtIndex(lastIndex);
-          return true;
+          return false;
         default:
           this.adapter_.focusOnElementAtIndex(index - 1);
-          return true;
+          return false;
       }
     }
 
-    if (isTab && index === lastIndex) {
-      // Restore Focus to Input
-      this.adapter_.focusOnInput();
-      this.close();
-      this.adapter_.notifyCancel();
-      return false;
-    }
-
-    return false;
+    // fallback
+    return true;
   }
 
-  handleItemKeyUp(event) {
+  handleItemKeyUp_(event) {
     // Do nothing if Alt, Ctrl or Meta are pressed.
     if (event.altKey || event.ctrlKey || event.metaKey) {
       return true;
@@ -274,18 +182,14 @@ class DatalistFoundation extends MDCFoundation {
 
     // If we press Enter or Space, then we select the item
     if (isEnter || isSpace) {
-      // Restore Focus to Input
-      this.adapter_.focusOnInput();
-      this.close();
+      this.close(true);
       this.adapter_.selectItemAtIndex(index);
       return false;
     }
 
     // Esc is a cancel-type action
     if (isEscape) {
-      // Restore Focus to Input
-      this.adapter_.focusOnInput();
-      this.close();
+      this.close(true);
       this.adapter_.notifyCancel();
       return false;
     }
@@ -293,9 +197,24 @@ class DatalistFoundation extends MDCFoundation {
     return true;
   }
 
-  handleDocumentClick() {
-    this.close();
+  handleItemClick_(event) {
+    // We don't want the click event to bubble up
+    event.stopPropagation();
 
+    const index = this.adapter_.getIndexForEventTarget(event.target);
+
+    // If an index value was returned that is AT LEAST 0, then we have
+    // an index and we'll select it!
+    if (index >= 0) {
+      this.close(true);
+      this.adapter_.selectItemAtIndex(index);
+    }
+
+    return true;
+  }
+
+  handleDocumentClick_() {
+    this.close();
     this.adapter_.notifyCancel();
   }
 }
--- src/foundations/DatalistFoundation.test.js
+++ src/foundations/DatalistFoundation.test.js
@@ -10,17 +10,12 @@ describe('DatalistFoundation', () => {
     adapter = {
       addClass: sinon.spy(),
       removeClass: sinon.spy(),
-      registerInputEventHandler: sinon.spy(),
-      deregisterInputEventHandler: sinon.spy(),
-      registerBtnClickHandler: sinon.spy(),
-      deregisterBtnClickHandler: sinon.spy(),
       registerInteractionEventHandler: sinon.spy(),
       deregisterInteractionEventHandler: sinon.spy(),
       registerDocumentClickHandler: sinon.spy(),
       deregisterDocumentClickHandler: sinon.spy(),
-      focusOnInput: sinon.spy(),
-      isInputFocused: sinon.stub(),
-      matchExists: sinon.stub(),
+      storeFocus: sinon.spy(),
+      restoreFocus: sinon.spy(),
       focusOnElementAtIndex: sinon.spy(),
       getIndexForEventTarget: sinon.stub(),
       getNumberOfItems: sinon.stub(),
@@ -40,16 +35,12 @@ describe('DatalistFoundation', () => {
     // We are going to use method names and the expect arguments they will be called
     // with to test and make sure that they all work
     let scenarios = [
-      ['registerInputEventHandler', 'click', foundation.inputClickHandler_],
-      ['registerInputEventHandler', 'keydown', foundation.inputKeyDownHandler_],
       ['registerInteractionEventHandler', 'click', foundation.itemClickHandler_],
-      ['registerInteractionEventHandler', 'keydown', foundation.itemKeydownHandler_],
-      ['registerInteractionEventHandler', 'keyup', foundation.itemKeyupHandler_],
-      ['deregisterInputEventHandler', 'click', foundation.inputClickHandler_],
-      ['deregisterInputEventHandler', 'keydown', foundation.inputKeyDownHandler_],
+      ['registerInteractionEventHandler', 'keydown', foundation.itemKeyDownHandler_],
+      ['registerInteractionEventHandler', 'keyup', foundation.itemKeyUpHandler_],
       ['deregisterInteractionEventHandler', 'click', foundation.itemClickHandler_],
-      ['deregisterInteractionEventHandler', 'keydown', foundation.itemKeydownHandler_],
-      ['deregisterInteractionEventHandler', 'keyup', foundation.itemKeyupHandler_],
+      ['deregisterInteractionEventHandler', 'keydown', foundation.itemKeyDownHandler_],
+      ['deregisterInteractionEventHandler', 'keyup', foundation.itemKeyUpHandler_],
     ];
 
     // All of the methods with two arguments will use this iterator
@@ -63,77 +54,163 @@ describe('DatalistFoundation', () => {
 
       expect(func.calledWith(arg1, arg2)).toEqual(true);
     });
-
-    // Now we are going to iterate over all the methods that take one arg
-    scenarios = [
-      ['registerBtnClickHandler', foundation.toggle],
-      ['deregisterBtnClickHandler', foundation.toggle],
-      ['registerDocumentClickHandler', foundation.documentClickHandler_],
-      ['deregisterDocumentClickHandler', foundation.documentClickHandler_],
-    ];
-
-    scenarios.forEach(scenario => {
-      const method = scenario[0];
-      const arg = scenario[1];
-
-      // Setting the func/stub found within the adapter object
-      const func = adapter[method];
-
-      expect(func.calledWith(arg)).toEqual(true);
-    });
   });
 
   it('has a default isOpen state of false', () => {
-    expect(foundation.isOpen()).toEqual(false);
+    expect(foundation.isOpen_).toEqual(false);
   });
 
   it('toggles between open and close', () => {
     foundation.open();
-    expect(foundation.isOpen()).toEqual(true);
+    expect(foundation.isOpen_).toEqual(true);
     expect(adapter.addClass.called).toEqual(true); // expect a class to be added
 
     foundation.close();
-    expect(foundation.isOpen()).toEqual(false);
+    expect(foundation.isOpen_).toEqual(false);
     expect(adapter.removeClass.called).toEqual(true); // expect a class to be removed
-    expect(adapter.focusOnInput.called).toEqual(true); // focuses on input again
 
     foundation.toggle();
-    expect(foundation.isOpen()).toEqual(true);
+    expect(foundation.isOpen_).toEqual(true);
 
     foundation.toggle();
-    expect(foundation.isOpen()).toEqual(false);
+    expect(foundation.isOpen_).toEqual(false);
+
+    // With our store function called too
+    foundation.open(true);
+    expect(foundation.isOpen_).toEqual(true);
+    expect(adapter.storeFocus.called).toEqual(true);
+
+    // With our restore function called too
+    foundation.close(true);
+    expect(foundation.isOpen_).toEqual(false);
+    expect(adapter.restoreFocus.called).toEqual(true);
   });
 
-  it('opens when the input is clicked and it is already focused', () => {
-    adapter.isInputFocused.returns(false);
-    foundation.inputClickHandler_();
-    expect(foundation.isOpen()).toEqual(false);
-
-    adapter.isInputFocused.returns(true);
-    foundation.inputClickHandler_();
-    expect(foundation.isOpen()).toEqual(true);
-  });
-
-  it('opens and closes if it detects input typing and checks matches', () => {
-    adapter.matchExists.returns(true);
-    foundation.inputKeydownHandler_({ key: 'Misc' });
-    expect(foundation.isOpen()).toEqual(true);
-
-    adapter.matchExists.returns(false);
-    foundation.inputKeydownHandler_({ key: 'Misc' });
-    expect(foundation.isOpen()).toEqual(false);
-  });
-
-  it('focuses on items at indexes if arrow key pressed from input', () => {
-    foundation.inputKeydownHandler_({ key: 'ArrowDown' });
+  it('focuses on specific indexes', () => {
+    foundation.focusOnItemAtIndex(0);
     expect(adapter.focusOnElementAtIndex.calledWith(0)).toEqual(true);
-    
-    adapter.getNumberOfItems.returns(2);
-    foundation.inputKeydownHandler_({ key: 'ArrowUp' });
+
+    foundation.focusOnItemAtIndex(1);
     expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
   });
 
+  it('does not cycle through focusing on items if certain keys are pressed', () => {
+    // Reset our spy call count
+    adapter.focusOnElementAtIndex.reset(); 
+
+    // Expect failure for the alt/ctrl/meta keys
+    expect(foundation.handleItemKeyDown_({ altKey: true })).toEqual(true);
+    expect(adapter.focusOnElementAtIndex.called).toEqual(false);
+    expect(foundation.handleItemKeyDown_({ ctrlKey: true })).toEqual(true);
+    expect(adapter.focusOnElementAtIndex.called).toEqual(false);
+    expect(foundation.handleItemKeyDown_({ metaKey: true })).toEqual(true);
+    expect(adapter.focusOnElementAtIndex.called).toEqual(false);
+  });
+
+  it('closes if tab is pressed and first or last item is triggering event', () => {
+    // Reset our spy call count and set the return to 0
+    adapter.focusOnElementAtIndex.reset();
+    adapter.restoreFocus.reset();
+    adapter.notifyCancel.reset();
+    adapter.getNumberOfItems.returns(3);
+
+    // Open the component and expect it to be open
+    foundation.open();
+    expect(foundation.isOpen_).toEqual(true);
+
+    // We are going to be located at the first index, then
+    // we are going to press a tab + shift, then
+    // we are going to close the component and restore focus
+    adapter.getIndexForEventTarget.returns(0);
+    foundation.handleItemKeyDown_({ key: 'Tab', shiftKey: true });
+    expect(foundation.isOpen_).toEqual(false);
+
+    // Open again, and change returned index to 2
+    adapter.getIndexForEventTarget.returns(2);
+    foundation.open();
+
+    // We are going to handle the item key down without shift
+    foundation.handleItemKeyDown_({ key: 'Tab' });
+    expect(foundation.isOpen_).toEqual(false);
+
+    // From both tests, we expect these functions to be called twice
+    expect(adapter.restoreFocus.calledTwice).toEqual(true);
+    expect(adapter.notifyCancel.calledTwice).toEqual(true);
+  });
+
+  it('cycles through the list of items on keyboard presses', () => {
+    // Now let's prepare our stubs...
+    adapter.focusOnElementAtIndex.reset();
+    adapter.getNumberOfItems.returns(3); // length will always be 3
+
+    // For the next couple tests, our current index will always be 0
+    adapter.getIndexForEventTarget.returns(0);
+
+    // Now we'll test ArrowDown, ArrowUp, Tab, Shift+Tab
+    foundation.handleItemKeyDown_({ key: 'ArrowDown' });
+    expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
+    foundation.handleItemKeyDown_({ key: 'ArrowUp' });
+    expect(adapter.focusOnElementAtIndex.calledWith(2)).toEqual(true);
+    foundation.handleItemKeyDown_({ key: 'Tab' });
+    expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
+
+    // Next we'll conduct the next few tests with the LAST INDEX, 2
+    adapter.getIndexForEventTarget.returns(2);
+
+    // Now we'll test keyDown and keyUp
+    foundation.handleItemKeyDown_({ key: 'ArrowDown' });
+    expect(adapter.focusOnElementAtIndex.calledWith(0)).toEqual(true);
+    foundation.handleItemKeyDown_({ key: 'ArrowUp' });
+    expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
+    foundation.handleItemKeyDown_({ key: 'Tab', shiftKey: true });
+    expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
+  });
+
+  it('does nothing if keydown event does not use known keys', () => {
+    // return 0 number of items
+    adapter.getNumberOfItems.returns(0);
+    expect(foundation.handleItemKeyDown_({ key: 'a' })).toEqual(true);
+
+    // return 1 number of items
+    adapter.getNumberOfItems.returns(1);
+    expect(foundation.handleItemKeyDown_({ key: 'a' })).toEqual(true);
+
+    // return 2 number of items
+    adapter.getNumberOfItems.returns(2);
+    expect(foundation.handleItemKeyDown_({ key: 'a' })).toEqual(true);
+  });
+
+  it('selects items at index if enter or space is pressed', () => {
+    const keys = ['Enter', 'Space'];
+
+    // We expect all of the keys to call selectItemAtIndex
+    keys.forEach(key => {
+      adapter.restoreFocus.reset();
+      adapter.selectItemAtIndex.reset();
+
+      // Pretend we pressed the esc button
+      foundation.handleItemKeyUp_({ key });
+
+      expect(adapter.restoreFocus.called).toEqual(true);
+      expect(adapter.selectItemAtIndex.called).toEqual(true);
+    });
+  });
+
+  it('cancels out if esc pressed', () => {
+    // Reset our spies
+    adapter.restoreFocus.reset();
+    adapter.notifyCancel.reset();
+
+    // Pretend we pressed the esc button
+    foundation.handleItemKeyUp_({ key: 'Escape' });
+
+    expect(adapter.notifyCancel.called).toEqual(true);
+  });
+
   it('selects item at an index when an item is clicked', () => {
+    // reset our spy
+    adapter.selectItemAtIndex.reset();
+
     // We're going to open the foundation for starters
     foundation.open();
 
@@ -143,11 +220,11 @@ describe('DatalistFoundation', () => {
     // If it's a -1, none of the proceeding functions will be called
     adapter.getIndexForEventTarget.returns(-1);
     // fire our click handler with a null target
-    foundation.itemClickHandler_({ target: null });
+    foundation.handleItemClick_({ target: null });
     // expect our adapter callback function to NOT be called
     expect(adapter.selectItemAtIndex.called).toEqual(false);
     // expect foundation to STILL remain open
-    expect(foundation.isOpen()).toEqual(true);
+    expect(foundation.isOpen_).toEqual(true);
 
     // We'll return the index of a hypothetical first item
     // This means our adapter DID FIND an index for the element
@@ -155,89 +232,20 @@ describe('DatalistFoundation', () => {
     // The proceeding functions should be called
     adapter.getIndexForEventTarget.returns(0);
     // fire our click handler with a null target (really doesn't matter)
-    foundation.itemClickHandler_({ target: null });
+    foundation.handleItemClick_({ target: null });
     // expect our adapter callback function to NOT be called
-    expect(adapter.selectItemAtIndex.called).toEqual(true);
+    expect(adapter.selectItemAtIndex.calledWith(0)).toEqual(true);
     // Now expect foundation to close
-    expect(foundation.isOpen()).toEqual(false);
-  });
-
-  it('cycles through the list of items on keyboard presses', () => {
-    // Reset our spy call count
-    adapter.focusOnElementAtIndex.reset(); 
-
-    // Expect failure for the alt/ctrl/meta keys
-    foundation.itemKeydownHandler_({ altKey: true });
-    expect(adapter.focusOnElementAtIndex.called).toEqual(false);
-    foundation.itemKeydownHandler_({ ctrlKey: true });
-    expect(adapter.focusOnElementAtIndex.called).toEqual(false);
-    foundation.itemKeydownHandler_({ metaKey: true });
-    expect(adapter.focusOnElementAtIndex.called).toEqual(false);
-
-    // Now let's prepare our stubs...
-    // The length will always be 3
-    adapter.getNumberOfItems.returns(3);
-
-    // For the next couple tests, our current index will always be 0
-    adapter.getIndexForEventTarget.returns(0);
-
-    // Now we'll test keyDown and keyUp
-    foundation.itemKeydownHandler_({ key: 'ArrowDown' });
-    expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
-    foundation.itemKeydownHandler_({ key: 'ArrowUp' });
-    expect(adapter.focusOnElementAtIndex.calledWith(2)).toEqual(true);
-
-    // Next we'll conduct the next few tests with the LAST INDEX, 2
-    adapter.getIndexForEventTarget.returns(2);
-
-    // Now we'll test keyDown and keyUp
-    foundation.itemKeydownHandler_({ key: 'ArrowDown' });
-    expect(adapter.focusOnElementAtIndex.calledWith(0)).toEqual(true);
-    foundation.itemKeydownHandler_({ key: 'ArrowUp' });
-    expect(adapter.focusOnElementAtIndex.calledWith(1)).toEqual(true);
-  });
-
-  it('selects items at index if tab, enter, or space is pressed', () => {
-    const keys = ['Tab', 'Enter', 'Space'];
-
-    // We expect all of the keys to call selectItemAtIndex
-    keys.forEach(key => {
-      adapter.selectItemAtIndex.reset();
-
-      // Pretend we pressed the esc button
-      foundation.itemKeyupHandler_({ key });
-
-      expect(adapter.selectItemAtIndex.called).toEqual(true);
-    });
-  });
-
-  it('cancels out if esc pressed', () => {
-    // Reset our spy
-    adapter.notifyCancel.reset();
-
-    // Pretend we pressed the esc button
-    foundation.itemKeyupHandler_({ key: 'Escape' });
-
-    expect(adapter.notifyCancel.called).toEqual(true);
-  });
-
-  it('closes the list on canceling out', () => {
-    // reset our spy
-    adapter.notifyCancel.reset();
-
-    // Cancel our foundation
-    foundation.cancel();
-
-    expect(adapter.notifyCancel.called).toEqual(true);
-    expect(foundation.isOpen()).toEqual(false);
+    expect(foundation.isOpen_).toEqual(false);
   });
 
   it('cancels when the document is clicked', () => {
     // reset our stub 
     adapter.notifyCancel.reset();
 
-    foundation.documentClickHandler_();
+    foundation.handleDocumentClick_();
 
+    expect(foundation.isOpen_).toEqual(false);
     expect(adapter.notifyCancel.called).toEqual(true);
   });
 });
