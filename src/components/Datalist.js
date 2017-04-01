ef2355aa8a97399dfa6fe4155532cf910d75515a
Marco Salazar de Leon <msalazardeleon@gmail.com>
2017-03-31 18:27:29 -0700

    Completed Datalist component; need to write tests and comments

--- src/components/Datalist.js
+++ src/components/Datalist.js
@@ -6,9 +6,6 @@ import DatalistFoundation from '../foundations/DatalistFoundation';
 // Adapter
 import datalistAdapter from '../adapters/datalistAdapter';
 
-// Component
-import SimpleMenuOption from './SimpleMenuOption';
-
 // Styles
 import '../styles/datalist.css';
 import '@material/list/dist/mdc.list.css';
@@ -18,8 +15,6 @@ class Datalist extends PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {};
-
     // Object that contains our option nodes
     this.optionNodes = [];
 
@@ -27,7 +22,6 @@ class Datalist extends PureComponent {
     this.setRootRef = this.setRootRef.bind(this);
     this.setListRef = this.setListRef.bind(this);
     this.getOptionNodes = this.getOptionNodes.bind(this);
-    this.renderOption = this.renderOption.bind(this);
 
     // Create our foundation
     this.foundation = new DatalistFoundation(datalistAdapter(this));
@@ -40,8 +34,16 @@ class Datalist extends PureComponent {
     this.foundation.init();
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps) {
     this.optionNodes = this.getOptionNodes();
+
+    if (prevProps.open !== this.props.open) {
+      if (this.props.open) {
+        this.foundation.open(true);
+      } else {
+        this.foundation.close();
+      }
+    }
   }
 
   componentWillUnmount() {
@@ -60,58 +62,38 @@ class Datalist extends PureComponent {
     return [].slice.call(this.rootNode.querySelectorAll('.mdc-list-item'));
   }
 
-  renderOption(option) {
-    this.optionObjects.push(option);
-
-    // Get the id and text from the option
-    const { id, text } = option;
-
-    return (<SimpleMenuOption id={id} key={id} text={text} />);
-  }
-
   render() {
-    let value = this.state.value;
+    function toRenderedItem(item) {
+      const { id, text } = item;
 
-    // If the value exists, we are going to lower case it
-    if (value) {
-      value = value.toLowerCase();
+      return (
+        <li className="mdc-list-item" id={id} key={id} tabIndex="0">
+          {text}
+        </li>
+      );
     }
 
-    const options = this.props.options.filter(option => {
-      const text = option.text.toLowerCase();
-
-      if (value) {
-        return (text.indexOf(value) !== -1);
-      }
-
-      return false;
-    });
-
-    // We are going to store the options array in this class
-    this.optionObjects = options;
-
     return (
-      <div>
-        <input
-          onChange={(event) => { this.setState({ value: event.target.value }); }}
-          type="text"
-          ref={(node) => { this.inputNode = node; }}
-        />
-        <button ref={(node) => { this.btnNode = node; }} tabIndex="-1">Click Me</button>
-        <div className="mdc-datalist" ref={this.setRootRef}>
-          <ul
-            aria-hidden="true"
-            className="mdc-list mdc-datalist__items"
-            ref={this.setListRef}
-            role="menu"
-            tabIndex="-1"
-          >
-            {options.map(this.renderOption)}
-          </ul>
-        </div>
+      <div className="mdc-datalist" ref={this.setRootRef}>
+        <ul
+          aria-hidden="true"
+          className="mdc-list mdc-datalist__items"
+          ref={this.setListRef}
+          role="menu"
+          tabIndex="-1"
+        >
+          {this.props.items.map(toRenderedItem)}
+        </ul>
       </div>
     );
   }
 }
+
+Datalist.propTypes = {
+  children: PropTypes.any.isRequired,
+  open: PropTypes.bool.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+};
 
 export default Datalist;
